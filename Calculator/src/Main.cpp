2c6d705e207fa69d09be90504fd8d5ebfdfe01f2
Latif Akan <latifakan1999@gmail.com>
2024-03-08 06:56:48 +0100

    added basic math calculator

---
+++ Calculator/src/Main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "ChooseOption.hpp"
+#include "MathCalculator.hpp"
+
+int main()
+{
+	Option User1;
+	User1.userSetChoice();
+	User1.checkUserChoice();
+
+	system("pause");
+	return 0;
+}
